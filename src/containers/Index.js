6803bba5f24650b393a4409b42699035161333a0
Elman Huseynov <thehuseyn0w@gmail.com>
2020-06-13 14:26:08 +0400

    Rewrite main components to hooks version

--- src/containers/Index.js
+++ src/containers/Index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import BackgroundAnimation from '../components/BackgroundAnimation';
 import Preloader from '../components/Preloader';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -8,28 +8,41 @@ import FormSection from '../components/Form';
 import axios from 'axios';
 
 
-class Index extends Component{
 
-    state = {
-        textField:'',
-        selectField:'.com',
+const Index = (props) => {
+    const [state, setState] = useState({
+        textField: '',
+        selectField: '.com',
         loaderStatus: false,
-        checkButtonDisabled:true,
-        domainfree:false,
-        registeredDate:'',
-        expirationDate:'',
-        showInfo:false
-    };
-    
-    DomenStatusUpdate = () => {
-        this.setState({
-            domainfree:true,
-            showInfo:true
+        checkButtonDisabled: true,
+        domainfree: false,
+        registeredDate: '',
+        expirationDate: '',
+        showInfo: false
+    });
+
+    useEffect(() => {
+        loadStatusUpdate();
+    }, []);
+
+    const loadStatusUpdate = () => {
+        setState({
+            ...state,
+            loaderStatus: true
         })
     }
 
-    DomainDataUpdate = (registerDate, expireDate) => {
-        this.setState({
+    const DomenStatusUpdate = () => {
+        setState({
+            ...state,
+            domainfree: true,
+            showInfo: true
+        })
+    }
+
+    const DomainDataUpdate = (registerDate, expireDate) => {
+        setState({
+            ...state,
             domainfree: false,
             showInfo: true,
             registeredDate: registerDate,
@@ -37,90 +50,83 @@ class Index extends Component{
         })
     }
 
-    loadStatusUpdate = () => {
-        this.setState({
-            loaderStatus: true
-        })
-    }
-
-    componentDidMount(){
-        this.loadStatusUpdate();
-    }
-
-    textFieldHandler = (e) =>{
+    const textFieldHandler = (e) => {
         var currentText = e.target.value;
         var trimmedString = currentText.replace(/\s/g, "");
-        if(e.target.value === ""){
-            this.setState({
+        if (e.target.value === "") {
+            setState({
+                ...state,
                 textField: trimmedString,
                 checkButtonDisabled: true
             })
-        }
-        else{
-            this.setState({
+        } else {
+            setState({
+                ...state,
                 textField: trimmedString,
                 checkButtonDisabled: false
             })
         }
-        
+
     }
 
-    selectFieldHandler = (e) => {
-        this.setState({
+    const checkStart = () => {
+        let domainName = state.textField;
+        let domainZone = state.selectField;
+        if (domainName !== "") {
+            setState({
+                ...state,
+                loaderStatus: false
+            });
+            let targetURL = "https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_j7mweyIXYnGCJIdPwG9b5amjqByet&domainName=" + domainName + domainZone + "&outputFormat=JSON";
+            axios.get(targetURL)
+                .then(function (response) {
+                    console.log(response);
+                    loadStatusUpdate();
+                    if (!response.data.WhoisRecord.createdDate) {
+                        DomenStatusUpdate();
+                    } else {
+                        let registeredDate = response.data.WhoisRecord.createdDate;
+                        if (response.data.WhoisRecord.createdDateNormalized){
+                            registeredDate = response.data.WhoisRecord.createdDateNormalized;
+                        }
+                        let expiredDate = response.data.WhoisRecord.createdDate;
+                        if(response.data.WhoisRecord.expiresDateNormalized){
+                            expiredDate = response.data.WhoisRecord.expiresDateNormalized;
+                        }
+                        DomainDataUpdate(registeredDate, expiredDate);
+                    }
+
+                })
+                .catch(function (error) {
+                    // handle error
+                    loadStatusUpdate();
+                    alert('There is some problem, please try again later');
+                })
+        } else {
+            alert('Please enter domain name!');
+        }
+    }
+
+    const selectFieldHandler = (e) => {
+        setState({
+            ...state,
             selectField: e.target.value
         })
     }
 
-    checkStart = () =>{
-        let domainName = this.state.textField;
-        let domainZone = this.state.selectField;
-        if(domainName !== ""){
-            this.setState({
-                loaderStatus: false
-            });
-            var rightContext = this;
-            let targetURL = "https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_j7mweyIXYnGCJIdPwG9b5amjqByet&domainName=" + domainName + domainZone + "&outputFormat=JSON";
-                axios.get(targetURL)
-                    .then(function (response) {
-                        rightContext.loadStatusUpdate();
-                        if(response.data.WhoisRecord.dataError === "MISSING_WHOIS_DATA"){
-                            rightContext.DomenStatusUpdate();
-                        }
-                        else{
-                            let registeredDate = response.data.WhoisRecord.createdDate;
-                            let expiredDate = response.data.WhoisRecord.createdDate;
-                            rightContext.DomainDataUpdate(registeredDate, expiredDate);
-                        }
-
-                    })
-                    .catch(function (error) {
-                        // handle error
-                        rightContext.loadStatusUpdate();
-                        alert('There is some problem, please try again later');
-                    })
-        }
-        else{
-            alert('Please enter domain name!');
-        }
-        
-
-
-    }
-
-    render(){
-        let info = this.state.showInfo
+    let info = state.showInfo
         ?
             <div className="container">
                  <div className="row">
                     <div className="col-12 text-center">
-                        {this.state.domainfree 
+                        {state.domainfree 
                         ?
                             <h2>This domain is Free! You can go and try to buy it!</h2>
                         :
                             <div>
                                 <h2>Unfortunately the domain is unavailable!</h2>
-                                <p>Registered Date: {this.state.registeredDate}</p>
-                                <p>Expiration Date: {this.state.expirationDate}</p>
+                                <p>Registered Date: {state.registeredDate}</p>
+                                <p>Expiration Date: {state.expirationDate}</p>
                             </div>
                         }
                     </div>
@@ -130,21 +136,21 @@ class Index extends Component{
 
         null;
 
-        return <div className="PageCover">
-            <BackgroundAnimation />
-            <Preloader hide={this.state.loaderStatus} />
+    
+    return <div className="PageCover">
+        <BackgroundAnimation />
+            <Preloader hide={state.loaderStatus} />
             <div className="PageContent">
                 <Header />
-                <FormSection inputChange={this.textFieldHandler} 
-                             selectChange={this.selectFieldHandler} 
-                             checkStart={this.checkStart} 
-                             checkButtonStatus={this.state.checkButtonDisabled}
+                <FormSection inputChange={textFieldHandler} 
+                             selectChange={selectFieldHandler} 
+                             checkStart={checkStart} 
+                             checkButtonStatus={state.checkButtonDisabled}
                 />
                 {info}
                 <Footer />
             </div>
-        </div>
-    }
+    </div>;
 }
 
 export default Index;
